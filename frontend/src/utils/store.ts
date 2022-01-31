b2967e78bdfc1c1c600a54a0b96c90bb6c42d290
Mathieu Veber <veberm8@gmail.com>
2022-01-31 15:48:13 +0100

    Deploy backend

--- frontend/src/utils/store.ts
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-
-import pageReducer from "../Page/reducer";
-
-export const store = configureStore({
-  reducer: { page: pageReducer },
-});
-
-export type RootState = ReturnType<typeof store.getState>;
